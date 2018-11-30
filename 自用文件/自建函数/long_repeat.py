4274261066535770236ae6561286f83ed5db6f23
cooper <huafengdongji@hotmail.com>
2018-11-30 20:33:50 +0800

    19:11

---
+++ 自用文件/自建函数/long_repeat.py
@@ -0,0 +1,35 @@
+import re
+
+
+def long_repeat(line):
+    """
+        length the longest substring that consists of the same char
+    """
+    letter = re.split(r'\B', line)
+    l = len(letter)
+    L = []
+    for x in range(l):
+        n = 0
+        a = letter[x]
+        for y in range(x, l):
+            b = letter[y]
+            # print(a, b)
+            if a == b == '':
+                return 0
+            elif a == b :
+                n += 1
+            else:
+                break
+        L.append(n)
+    L.sort(reverse=True)
+    return L[0]
+
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert long_repeat('sdsffffse') == 4, "First"
+    assert long_repeat('ddvvrwwwrggg') == 3, "Second"
+    assert long_repeat('abababaab') == 2, "Third"
+    assert long_repeat('') == 0, "Empty"
+    print('"Run" is good. How is "Check"?')
