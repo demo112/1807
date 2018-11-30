41d3d4dc50e8f500bb659a5fb2db3a2758665046
cooper <huafengdongji@hotmail.com>
2018-11-30 20:48:20 +0800

    20:48

--- 自用文件/自建函数/long_repeat.py
+++ 自用文件/自建函数/long_repeat.py
@@ -21,8 +21,7 @@ def long_repeat(line):
             else:
                 break
         L.append(n)
-    L.sort(reverse=True)
-    return L[0]
+    return max(L)
 
 
 
@@ -32,4 +31,15 @@ if __name__ == '__main__':
     assert long_repeat('ddvvrwwwrggg') == 3, "Second"
     assert long_repeat('abababaab') == 2, "Third"
     assert long_repeat('') == 0, "Empty"
+    print('"Run" is good. How is "Check"?')
+
+from itertools import groupby
+
+def long_repeat(line):
+    return max((sum(1 for _ in g) for k, g in groupby(line)), default=0)
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert long_repeat('sdsffffse') == 4, "First"
+    assert long_repeat('ddvvrwwwrggg') == 3, "Second"
     print('"Run" is good. How is "Check"?')
