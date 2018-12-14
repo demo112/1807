039ceb3495164dc6661e30d1203cc8370951c95f
cooper <huafengdongji@hotmail.com>
2018-12-14 08:59:32 +0800

    1213

--- 自用文件/自建函数/第二个岛/括号合法判断.py
+++ 自用文件/自建函数/第二个岛/括号合法判断.py
@@ -1,33 +1,26 @@
-def checkio(expression):
-    left = []
-    right = []
-    for i in expression:
-        if i in ("(", "[", "{"):
-            if i == '(':
-                left.insert(0, 's')
-            if i == '[':
-                left.insert(0, 'm')
-            if i == '{':
-                left.insert(0, 'l')
-        elif i in (")", "]", "}"):
-            if i == ')':
-                right.insert(0, 's')
-            if i == ']':
-                right.insert(0, 'm')
-            if i == '}':
-                right.insert(0, 'l')
-    print(left, right)
-    right.reverse()
-    if left == right:
-        return True
-    else:
-        return False
+import re
 
-#These "asserts" using only for self-checking and not necessary for auto-testing
-if __name__ == '__main__':
-    assert checkio("((5+3)*2+1)") == True, "Simple"
-    assert checkio("{[(3+1)+2]+}") == True, "Different types"
-    assert checkio("(3+{1-1)}") == False, ") is alone inside {}"
-    assert checkio("[1+1]+(2*2)-{3/3}") == True, "Different operators"
-    assert checkio("(({[(((1)-2)+3)-3]/3}-3)") == False, "One is redundant"
-    assert checkio("2+3") == True, "No brackets, no problem"
+def checkio(expression):
+    print(11)
+    res = r"\w*(?P<left>(|[|{)\w*(?P<right>)|]|})\w*"
+    res = r"\(\w+\)"
+    rem = r"\w*(\w*)\w*"
+    rel = r"\w*(\w*)\w*"
+    objs = re.match(res, expression)
+    i[1].upper()
+    print(objs.group())
+    # objm = re.findall(rem, expression)
+    # objl = re.findall(rel, expression)
+
+
+expression = "((5+3)*2+1)"
+checkio(expression)
+# These "asserts" using only for self-checking and not necessary for auto-testing
+
+# if __name__ == '__main__':
+    # assert checkio("((5+3)*2+1)") == True, "Simple"
+    # assert checkio("{[(3+1)+2]+}") == True, "Different types"
+    # assert checkio("(3+{1-1)}") == False, ") is alone inside {}"
+    # assert checkio("[1+1]+(2*2)-{3/3}") == True, "Different operators"
+    # assert checkio("(({[(((1)-2)+3)-3]/3}-3)") == False, "One is redundant"
+    # assert checkio("2+3") == True, "No brackets, no problem"
